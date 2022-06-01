2045e35cd240311fb829bdbe21381923177c45b1
Danylo Strus <danylo.strus@gmail.com>
2022-06-01 22:31:25 +0300

    little change in the branch

--- HW_0106/Program.cs
+++ HW_0106/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Create repos!");
 Console.WriteLine("Create repos and banch!");
+Console.WriteLine("a little change in the branch");
 
